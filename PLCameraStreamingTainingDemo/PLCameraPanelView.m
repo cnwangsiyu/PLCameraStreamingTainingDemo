b86cb135636c4e78b99708a319989f5584cf428c
moskize <xiangtantaozeyu@icloud.com>
2016-05-19 13:40:32 +0800

    refactor: add more switch for panel.

--- PLCameraStreamingTainingDemo/PLCameraPanelView.m
+++ PLCameraStreamingTainingDemo/PLCameraPanelView.m
@@ -21,23 +21,9 @@
         
         self.backgroundColor = [UIColor whiteColor];
         
-        _captureSwitch = ({
-            UISwitch *switchBar = [[UISwitch alloc] init];
-            UILabel *label = [[UILabel alloc] init];
-            [self addSubview:switchBar];
-            [self addSubview:label];
-            [switchBar setOn:YES];
-            [label setText:@"摄像头开关"];
-            [switchBar mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.left.equalTo(self).with.offset(10);
-                make.bottom.equalTo(self).with.offset(-10);
-            }];
-            [label mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.left.equalTo(switchBar.mas_right);
-                make.bottom.equalTo(switchBar.mas_bottom);
-            }];
-            switchBar;
-        });
+        NSArray *switches = [self _generateSwitchesWithTitles:@[@"摄像头开关"]];
+        
+        _captureSwitch = switches[0];
         
         _cameraContainerView = ({
             UIView *container = [[UIView alloc] init];
@@ -52,6 +38,42 @@
         });
     }
     return self;
+}
+
+- (NSArray *)_generateSwitchesWithTitles:(NSArray *)titles
+{
+    UISwitch *previousSwitch = nil;
+    NSMutableArray *switches = [[NSMutableArray alloc] initWithCapacity:titles.count];
+    
+    for (NSString *title in titles) {
+        UISwitch *switchBar = [self _generateSwitchWithTitle:title previousSwitch:previousSwitch];
+        previousSwitch = switchBar;
+        [switches addObject:switchBar];
+    }
+    return switches;
+}
+
+- (UISwitch *)_generateSwitchWithTitle:(NSString *)title previousSwitch:(UISwitch *)previousSwitch
+{
+    UISwitch *switchBar = [[UISwitch alloc] init];
+    UILabel *label = [[UILabel alloc] init];
+    [self addSubview:switchBar];
+    [self addSubview:label];
+    [switchBar setOn:YES];
+    [label setText:title];
+    [switchBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        if (previousSwitch) {
+            make.left.equalTo(previousSwitch.mas_right).with.offset(5);
+        } else {
+            make.left.equalTo(self).with.offset(10);
+        }
+        make.bottom.equalTo(self).with.offset(-10);
+    }];
+    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(switchBar.mas_right);
+        make.bottom.equalTo(switchBar.mas_bottom);
+    }];
+    return switchBar;
 }
 
 - (void)layoutSubviews
